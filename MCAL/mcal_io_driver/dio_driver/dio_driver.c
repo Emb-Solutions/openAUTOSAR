fd32943cbd01f7f422768b0810b16f219125468e
Sourav Dey <sourav.dey@fluxauto.xyz>
2024-11-15 11:52:36 +0530

    feat: Adding the MCAL layer/dio_driver

---
+++ MCAL/mcal_io_driver/dio_driver/dio_driver.c
@@ -0,0 +1,4 @@
+/* File Name: dio_driver.c
+   Date:      15th Nov 2024
+   Author:    Sourav Dey
+   Description: Specification of DIO Driver.*/
---
+++ MCAL/mcal_io_driver/dio_driver/dio_driver.h
@@ -0,0 +1,4 @@
+/* File Name: dio_driver.h
+   Date:      15th Nov 2024
+   Author:    Sourav Dey
+   Description: Header file for DIO Driver.*/
