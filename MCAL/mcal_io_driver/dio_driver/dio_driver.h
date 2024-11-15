e6199269864f4610c941df6b064aa57f9c223ec6
Sourav Dey <sourav.dey@fluxauto.xyz>
2024-11-15 15:30:13 +0530

    feat: Added the std include files.

--- MCAL/mcal_io_driver/dio_driver/dio_driver.h
+++ MCAL/mcal_io_driver/dio_driver/dio_driver.h
@@ -2,3 +2,16 @@
    Date:      15th Nov 2024
    Author:    Sourav Dey
    Description: Header file for DIO Driver.*/
+
+
+/* Include files*/
+#include "../../../include/std/std_types.h"
+
+typedef enum{
+   Dio_Channel1,
+   Dio_Channel2,
+   Dio_Channel3
+
+}Dio_ChannelType;
+
+
---
+++ include/std/std_types.h
@@ -0,0 +1,11 @@
+/*File Name: std_types.h
+   Date:     15th Nov 2024
+   Author:   Sourav Dey
+   Description: Header file for standard types.
+   */
+
+/* Global conventions*/
+# define UINT_8_T    uint8_t
+# define UINT_16_T   uint16_t
+# define UINT_32_T   uint32_t
+# define UINT_64_T   uint64_t
