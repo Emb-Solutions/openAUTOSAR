1a32af9b6296036740e2204b60c12fb290388e37
Subhajit Roy <subhajitroy005@gmail.com>
2024-12-09 20:43:07 +0530

    feat: Makefile base for compilation crelated.

--- MCAL/mcal.mk
+++ MCAL/mcal.mk
@@ -1,9 +1,6 @@
 
 -include ../build/make_config.mk
 
-.PHONY: device_sources_build
-
-
 
 #-----------------------------------------------
 #          MCAL Source files
@@ -21,12 +18,10 @@ MCAL_OBJS += \
 ../build_artifacts/stm32_f429xx_startup.o \
 ../build_artifacts/stm32_f429xx_irq.o
 
+
 device_sources_build: $(MCAL_OBJS)
-	@echo 'Finished Building!'
 
-
-
-$(MCAL_OBJS): $(MCAL_SRCS) ./mcal.mk
+$(MCAL_OBJS): $(MCAL_SRCS)
 	$(CC) "$<" -o "$@"
 
 
